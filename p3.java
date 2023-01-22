99cc9535ed73ac14c94a88dc7d8a14fe52228738
Satyam Kumar <satyam8804378323@gmail.com>
2023-01-23 01:09:38 +0530

    new commit

---
+++ p3.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Random.*;
+public class p3 {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        int randomNum = rnd.nextInt(20);
+
+        for (int i = 0; i < 6; i++)
+         {
+            System.out.println("Guess the Number :");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n>randomNum){
+            System.out.println("You guess is too high  !!");
+        }else if(n<randomNum){
+            System.out.println("You guess is too low !!");
+        }else{
+            System.out.println("you win !!");
+            break;
+        }
+        }
+        
+    }
+}
