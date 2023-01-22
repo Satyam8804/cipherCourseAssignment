aa7a893436b278e7c3c69374c9bbb696a1a70bf6
Satyam Kumar <satyam8804378323@gmail.com>
2023-01-23 01:14:42 +0530

    new

---
+++ p4.java
@@ -0,0 +1,14 @@
+public class p4 {
+    public static void main(String[] args) {
+        char ch='A';
+        while (ch<=90) {
+            System.out.println(ch+" ");
+            ch++;
+        }
+        char ch1 = 'A';
+        do {
+            System.out.println(ch1+" ");
+            ch1++;
+        } while (ch1<='Z');
+    }
+}
